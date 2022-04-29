bfe1f3c220101a96ce1351b0aa2baa3fa756c472
Oluwajuwon Olutomisin <jolutomisin@gmail.com>
2022-04-29 22:01:19 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const tabsMobile = document.querySelectorAll('.tabs-mobile p');
 
 //add todo to html
 const addTodo = todo => {
-    const html = `<li>
+    const html = `<li class="drag" draggable="true">
     <div class="flex">
       <div class="circle"><img src="./images/icon-check.svg" alt="" class="check-image"></div>
     <span>${todo}</span>
@@ -114,4 +114,26 @@ tabs.forEach(tab => {
    });
 });
 
-//drag and drop
+tabsMobile.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabsMobile.forEach(e => {
+            e.classList.remove('active')
+        });
+        tab.classList.add('active');
+        if(tab.textContent === "Active"){
+            filterActive();
+         } else if(tab.textContent === "Completed"){
+            filterCompleted();
+         }else {
+             children.forEach(child => {
+                 child.style.display = "flex"
+             })
+         }
+    });
+ });
+
+ // drag and drop using sortable.js
+
+ new Sortable(ul, {
+     animation: 300
+ });
